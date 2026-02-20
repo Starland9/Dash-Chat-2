7c573c091f5578d3e19764978608e8614a4dc968
Landry Simo <landrysimo99@gmail.com>
2026-02-20 16:26:57 +0100

    chore: update dependencies in pubspec.yaml
    - upgraded audioplayers from ^5.0.0 to ^6.5.1
    - upgraded file_picker from ^6.1.1 to ^10.3.10
    - upgraded permission_handler from ^11.3.0 to ^12.0.1
    - upgraded device_info_plus from ^9.1.2 to ^11.5.0
    - upgraded camera from ^0.10.5+9 to ^0.11.4
    - upgraded image_picker from ^1.0.7 to ^1.2.1
    - updated chat_input git repository URL
    - upgraded flutter_lints from ^3.0.1 to ^6.0.0

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,9 +7,9 @@ import Foundation
 
 import audioplayers_darwin
 import device_info_plus
+import file_picker
 import file_selector_macos
-import path_provider_foundation
-import record_darwin
+import record_macos
 import sqflite_darwin
 import url_launcher_macos
 import video_compress
@@ -18,9 +18,9 @@ import video_player_avfoundation
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   VideoCompressPlugin.register(with: registry.registrar(forPlugin: "VideoCompressPlugin"))
